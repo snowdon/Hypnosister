5c557a708e4c759f8379a523764ff8498c1cda73
Xiaochun Shen <snowdonshen@gmail.com>
2011-08-29 13:02:13 +0800

    Initial Commit

---
+++ Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by  Chuns on 11-8-29.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end

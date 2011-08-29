2602495c570ad156eacf569c6d3852d8dfb6a093
Xiaochun Shen <snowdonshen@gmail.com>
2011-08-29 13:04:08 +0800

    add HypnosisView

---
+++ HypnosisView.m
@@ -0,0 +1,14 @@
+//
+//  HypnosisView.m
+//  Hypnosister
+//
+//  Created by  Chuns on 11-8-29.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "HypnosisView.h"
+
+
+@implementation HypnosisView
+
+@end
